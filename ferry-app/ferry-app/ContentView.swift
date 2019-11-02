10050b357c3e78c5c26b3e80dfe66571ca63f09a
Sydnee Stanley <s.stanley@berkeley.edu>
2019-11-02 15:32:19 +0100

    create project

---
+++ ferry-app/ferry-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ferry-app
+//
+//  Created by Sydnee Stanley on 11/2/19.
+//  Copyright © 2019 Sydnee Stanley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
